efc5e4072234e01dff634accbce723caaa59e6b0
Vrknbby <165991002+Vrknbby@users.noreply.github.com>
2024-10-30 17:29:36 -0500

    Add files via upload

---
+++ src/main/java/pe/idat/RetoGit/RetoGitApplication.java
@@ -0,0 +1,13 @@
+package pe.idat.RetoGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoGitApplication.class, args);
+	}
+
+}
