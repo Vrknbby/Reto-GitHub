2de7065f5cc1e6d3ee98464bc5d752c91bee2d70
Vrknbby <barrantesnicolas27@gmail.com>
2024-10-30 18:22:36 -0500

    Cambios en alumnos.java

--- src/main/java/pe/idat/RetoGit/model/alumnos.java
+++ src/main/java/pe/idat/RetoGit/model/alumnos.java
@@ -1,7 +1,12 @@
 package pe.idat.RetoGit.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
 import java.util.Date;
 
+@Table
+@Entity(name="Alumnos")
 public class alumnos {
 
     Long idAlumno;
