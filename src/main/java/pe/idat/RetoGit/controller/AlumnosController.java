75413313261ec23d203c056e6c5ffa2136414eea
Vrknbby <barrantesnicolas27@gmail.com>
2024-10-30 18:59:35 -0500

    Update a controllerAlumnos

--- src/main/java/pe/idat/RetoGit/controller/AlumnosController.java
+++ src/main/java/pe/idat/RetoGit/controller/AlumnosController.java
@@ -2,21 +2,26 @@ package pe.idat.RetoGit.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pe.idat.RetoGit.model.Alumnos;
-import pe.idat.RetoGit.repository.AlumnosRepository;
+import pe.idat.RetoGit.service.AlumnosService;
 
 import java.util.ArrayList;
 
-@Controller
-@RequestMapping("/api/alumno")
+@RestController
+@RequestMapping("/api/alumnos")
 public class AlumnosController {
-
     @Autowired
-    AlumnosRepository alumnosRepository;
+    AlumnosService alumnosService;
 
+    @GetMapping()
+    public ArrayList<Alumnos> obtenerAlumnos(){
+        return alumnosService.listarAlumnos();
+    }
 
-
+    @PostMapping()
+    public Alumnos guardarAlumno(@RequestBody Alumnos alumno){
+        return this.alumnosService.guardarAlumno(alumno);
+    }
 
 }
