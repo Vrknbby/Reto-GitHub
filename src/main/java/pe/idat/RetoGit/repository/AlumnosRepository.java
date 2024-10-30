896ae39b4659baba6e928688659e1197637df655
Vrknbby <barrantesnicolas27@gmail.com>
2024-10-30 18:43:58 -0500

    Creacion de metodo GuardarAlumno

--- src/main/java/pe/idat/RetoGit/repository/AlumnosRepository.java
+++ src/main/java/pe/idat/RetoGit/repository/AlumnosRepository.java
@@ -1,7 +1,9 @@
 package pe.idat.RetoGit.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import pe.idat.RetoGit.model.Alumnos;
 
 @Repository
-public interface AlumnosRepository {
+public interface AlumnosRepository extends JpaRepository<Alumnos,Long> {
 }
--- src/main/java/pe/idat/RetoGit/service/AlumnosService.java
+++ src/main/java/pe/idat/RetoGit/service/AlumnosService.java
@@ -1,7 +1,23 @@
 package pe.idat.RetoGit.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pe.idat.RetoGit.model.Alumnos;
+import pe.idat.RetoGit.repository.AlumnosRepository;
 
 @Service
 public class AlumnosService {
+
+    @Autowired
+    AlumnosRepository alumnosRepository;
+
+    public Alumnos guardarAlumno(Alumnos Newalumno){
+        Alumnos alumno = new Alumnos(
+                Newalumno.getNombre(),
+                Newalumno.getApellidos(),
+                Newalumno.getFechaNac(),
+                Newalumno.getColegio()
+        );
+        return  alumnosRepository.save(alumno);
+    }
 }
